6d575c755215c8fe8fdb9323c7599e6495882ae7
sk8er123 <sk8er12379@gmail.com>
2023-12-26 22:08:51 +0900

    Update TokenLock.sol

--- mint/TokenLock.sol
+++ mint/TokenLock.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "Ownable.sol";
-import "IERC20.sol";
-import "TokenLockConst.sol";
-import "ITokenLock.sol";
+import "./Ownable.sol";
+import "./IERC20.sol";
+import "./TokenLockConst.sol";
+import "./ITokenLock.sol";
 
 contract TokenLock is Ownable, TokenLockConst, ITokenLock {
 
